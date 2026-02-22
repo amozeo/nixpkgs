6a13aba61bec0587350c8a34c79891bd83c1fc46
Wroclaw <wroclaw223@outlook.com>
2026-02-22 20:20:18 +0100

    qimgv: add update script

--- pkgs/by-name/qi/qimgv/package.nix
+++ pkgs/by-name/qi/qimgv/package.nix
@@ -3,11 +3,16 @@
   stdenv,
   fetchFromGitHub,
   cmake,
+  common-updater-scripts,
+  curl,
   exiv2,
+  jq,
   mpv,
+  nix-update,
   opencv4,
   pkg-config,
   qt6,
+  writeShellScript,
 }:
 
 stdenv.mkDerivation {
@@ -44,6 +49,23 @@ stdenv.mkDerivation {
   postPatch = ''
     substituteInPlace qimgv/settings.cpp \
       --replace-fail '"/usr/bin/mpv"' '"'${lib.escapeShellArg (lib.getExe mpv)}'"'
+  '';
+
+  # qimgv's git latest tag has `-alpha` suffix which we don't want to put in version
+  passthru.updateScript = writeShellScript "update-qimgv" ''
+    ${lib.getExe nix-update} qimgv --version branch=HEAD
+
+    latestTag=$(
+      ${lib.getExe curl} -s "https://api.github.com/repos/easymodo/qimgv/tags?per_page=1" \
+        | ${lib.getExe jq} -r '.[0].name | ltrimstr("v") | split("-") | .[0]'
+    )
+    latestDate=$(
+      ${lib.getExe curl} -s "https://api.github.com/repos/easymodo/qimgv/commits/HEAD" \
+        | ${lib.getExe jq} -r '.commit.committer.date | .[0:10]'
+    )
+
+    ${lib.getExe' common-updater-scripts "update-source-version"} qimgv \
+      "''${latestTag}-unstable-''${latestDate}" --ignore-same-hash
   '';
 
   meta = {
